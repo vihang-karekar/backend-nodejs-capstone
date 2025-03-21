6f990f1b23aac7b4941902a6108b5218e643cf2f
vihang-karekar <72259561+vihang-karekar@users.noreply.github.com>
2025-03-21 19:10:05 +0530

    Update authRoutes.js

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -1,44 +1,44 @@
 /* jshint esversion: 8 */
-const express = require('express');
-const bcryptjs = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const connectToDatabase = require('../models/db');
-const router = express.Router();
-const dotenv = require('dotenv');
-const pino = require('pino');  // Import Pino logger
+const express = require('express')
+const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const connectToDatabase = require('../models/db')
+const router = express.Router()
+const dotenv = require('dotenv')
+const pino = require('pino')  // Import Pino logger
 
 //Task 1: Use the `body`,`validationResult` from `express-validator` for input validation
-const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require('express-validator')
 
 
-const logger = pino();  // Create a Pino logger instance
+const logger = pino()  // Create a Pino logger instance
 
-dotenv.config();
+dotenv.config()
 const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post('/register', async (req, res) => {
     try {
       //Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
-      const db = await connectToDatabase();
-      const collection = db.collection("users");
-      const existingEmail = await collection.findOne({ email: req.body.email });
+      const db = await connectToDatabase()
+      const collection = db.collection("users")
+      const existingEmail = await collection.findOne({ email: req.body.email })
 
         if (existingEmail) {
-            logger.error('Email id already exists');
-            return res.status(400).json({ error: 'Email id already exists' });
+            logger.error('Email id already exists')
+            return res.status(400).json({ error: 'Email id already exists' })
         }
 
-        const salt = await bcryptjs.genSalt(10);
-        const hash = await bcryptjs.hash(req.body.password, salt);
+        const salt = await bcryptjs.genSalt(10)
+        const hash = await bcryptjs.hash(req.body.password, salt)
         const email=req.body.email;
-        console.log('email is',email);
+        console.log('email is',email)
         const newUser = await collection.insertOne({
             email: req.body.email,
             firstName: req.body.firstName,
             lastName: req.body.lastName,
             password: hash,
             createdAt: new Date(),
-        });
+        })
 
         const payload = {
             user: {
@@ -46,29 +46,29 @@ router.post('/register', async (req, res) => {
             },
         };
 
-        const authtoken = jwt.sign(payload, JWT_SECRET);
-        logger.info('User registered successfully');
-        res.json({ authtoken,email });
+        const authtoken = jwt.sign(payload, JWT_SECRET)
+        logger.info('User registered successfully')
+        res.json({ authtoken,email })
     } catch (e) {
-        logger.error(e);
-        return res.status(500).send('Internal server error');
+        logger.error(e)
+        return res.status(500).send('Internal server error')
     }
-});
+})
 
 router.post('/login', async (req, res) => {
     console.log("\n\n Inside login")
 
     try {
-        // const collection = await connectToDatabase();
-        const db = await connectToDatabase();
-        const collection = db.collection("users");
-        const theUser = await collection.findOne({ email: req.body.email });
+        // const collection = await connectToDatabase()
+        const db = await connectToDatabase()
+        const collection = db.collection("users")
+        const theUser = await collection.findOne({ email: req.body.email })
 
         if (theUser) {
             let result = await bcryptjs.compare(req.body.password, theUser.password)
             if(!result) {
-                logger.error('Passwords do not match');
-                return res.status(404).json({ error: 'Wrong pasword' });
+                logger.error('Passwords do not match')
+                return res.status(404).json({ error: 'Wrong pasword' })
             }
             let payload = {
                 user: {
@@ -79,60 +79,60 @@ router.post('/login', async (req, res) => {
             const userName = theUser.firstName;
             const userEmail = theUser.email;
 
-            const authtoken = jwt.sign(payload, JWT_SECRET);
-            logger.info('User logged in successfully');
-            return res.status(200).json({ authtoken, userName, userEmail });
+            const authtoken = jwt.sign(payload, JWT_SECRET)
+            logger.info('User logged in successfully')
+            return res.status(200).json({ authtoken, userName, userEmail })
         } else {
-            logger.error('User not found');
-            return res.status(404).json({ error: 'User not found' });
+            logger.error('User not found')
+            return res.status(404).json({ error: 'User not found' })
         }
     } catch (e) {
-        logger.error(e);
-        return res.status(500).json({ error: 'Internal server error', details: e.message });
+        logger.error(e)
+        return res.status(500).json({ error: 'Internal server error', details: e.message })
       }
-});
+})
 
 // update API
 router.put('/update', async (req, res) => {
     // Task 2: Validate the input using `validationResult` and return approiate message if there is an error.
 
-    const errors = validationResult(req);
+    const errors = validationResult(req)
 
     // Task 3: Check if `email` is present in the header and throw an appropriate error message if not present.
     if (!errors.isEmpty()) {
-        logger.error('Validation errors in update request', errors.array());
-        return res.status(400).json({ errors: errors.array() });
+        logger.error('Validation errors in update request', errors.array())
+        return res.status(400).json({ errors: errors.array() })
     }
 
     try {
         const email = req.headers.email;
 
         if (!email) {
-            logger.error('Email not found in the request headers');
-            return res.status(400).json({ error: "Email not found in the request headers" });
+            logger.error('Email not found in the request headers')
+            return res.status(400).json({ error: "Email not found in the request headers" })
         }
 
         //Task 4: Connect to MongoDB
-        const db = await connectToDatabase();
-        const collection = db.collection("users");
+        const db = await connectToDatabase()
+        const collection = db.collection("users")
 
         //Task 5: Find user credentials
-        const existingUser = await collection.findOne({ email });
+        const existingUser = await collection.findOne({ email })
 
         if (!existingUser) {
-            logger.error('User not found');
-            return res.status(404).json({ error: "User not found" });
+            logger.error('User not found')
+            return res.status(404).json({ error: "User not found" })
         }
 
         existingUser.firstName = req.body.name;
-        existingUser.updatedAt = new Date();
+        existingUser.updatedAt = new Date()
 
         //Task 6: Update user credentials in DB
         const updatedUser = await collection.findOneAndUpdate(
             { email },
             { $set: existingUser },
             { returnDocument: 'after' }
-        );
+        )
 
         //Task 7: Create JWT authentication with user._id as payload using secret key from .env file
         const payload = {
@@ -141,13 +141,13 @@ router.put('/update', async (req, res) => {
             },
         };
 
-        const authtoken = jwt.sign(payload, JWT_SECRET);
-        logger.info('User updated successfully');
+        const authtoken = jwt.sign(payload, JWT_SECRET)
+        logger.info('User updated successfully')
 
-        res.json({ authtoken });
+        res.json({ authtoken })
     } catch (error) {
-        logger.error(error);
-        return res.status(500).send("Internal Server Error");
+        logger.error(error)
+        return res.status(500).send("Internal Server Error")
     }
-});
+})
 module.exports = router;
