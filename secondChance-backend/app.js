738a66d760ab89f47a0a5aa4f1ab28bc587f0321
vihang-karekar <72259561+vihang-karekar@users.noreply.github.com>
2025-03-21 17:33:56 +0530

    Update app.js - lint 1

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -1,11 +1,11 @@
 /*jshint esversion: 8 */
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const pinoLogger = require('./logger');
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const pinoLogger = require('./logger')
 
-const connectToDatabase = require('./models/db');
-const {loadData} = require("./util/import-mongo/index");
+const connectToDatabase = require('./models/db')
+const { loadData } = require('./util/import-mongo/index');
 
 
 const app = express();
